21cfa8477da55b10865e4d2376da3ecfa12d9a59
kimmo <hemakim0215@naver.com>
2025-02-08 11:23:38 +0900

    .

--- back/AgitBack/user/UserManager.cs
+++ back/AgitBack/user/UserManager.cs
@@ -8,6 +8,7 @@ namespace AgitBack.UserManager
 
     public class AgitUserManager
     {
+        const int UID_LENGTH = 30;
         public int userNum {get; set;}
         // Search Containers
         private List<User> userContainer = new List<User>();
@@ -26,26 +27,65 @@ namespace AgitBack.UserManager
 
         public string MakeUID()
         {
-            string _newUID = "";
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            Random random = new Random();
+            char[] stringChars = new char[UID_LENGTH];
 
-            return _newUID;
+            for (int i = 0; i < UID_LENGTH; i++) {
+                // chars에서 랜덤한 문자 선택
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            // 문자 배열을 문자열로 변환
+            return new string(stringChars);
+        }
+
+        public bool IsExistUID(string _uid)
+        {
+            // 나중에는 DB에서 체크해야함
+
+            // DB 연동전이라서 서버에서 UID 중복검사 진행함
+            return this.userUIDSearchContainer.ContainsKey(_uid);
         }
 
         public void SignUpNewUser(AccountInfo _singupInfo)
         {
+            // Make UID
+            string _newUID = "";
+            while(true) {
+                _newUID = MakeUID();
+                if(false == IsExistUID(_newUID)) {
+                    break;
+                }
+            }
+            AgitUserServerInfo _userServerInfo = new AgitUserServerInfo();
+            _userServerInfo.serverId = -1; // 접속전이기 때문에
+            _userServerInfo.uid = _newUID;
 
+            User _newUser = new User(_userServerInfo, _singupInfo);
+
+            // CreateNewUser(_newUser); DB 연동후에는 DB에 저장함
+            EnterNewUser(_newUser); // DB 연동전이기 때문에 등록과 동시에 로그인(나중에 분리해야함)
         }
 
-        public void AddNewUser(User _newUser)
+        public void CreateNewUser(User _newUser)
         {
-            userContainer.Add(_newUser);
-            
-            userUIDSearchContainer.Add(_newUser.userServerInfo.uid, _newUser);
-            userIdSearchContainer.Add(_newUser.userAccountInfo.userID, _newUser);
-            userServerIndexSearchContainer.Add(_newUser.userServerInfo.serverId, _newUser);
-            userUserNameSearchContainer.Add(_newUser.userAccountInfo.userChatName, _newUser);
+            // DB에 저장            
+        }
 
-            userNum = this.userContainer.Count();
+        public void EnterNewUser(User _newUser)
+        {
+            this.userContainer.Add(_newUser);
+            
+            this.userUIDSearchContainer.Add(_newUser.userServerInfo.uid, _newUser);
+            this.userIdSearchContainer.Add(_newUser.userAccountInfo.userID, _newUser);
+
+            this.userUserNameSearchContainer.Add(_newUser.userAccountInfo.userChatName, _newUser);
+
+            this.userServerIndexSearchContainer.Add(userNum, _newUser);
+            _newUser.userServerInfo.serverId = userNum;
+
+            this.userNum = this.userContainer.Count();
         }
 
     }
