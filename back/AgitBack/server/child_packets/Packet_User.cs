0db1b57264ba40caa3b953ca4f3e506aee751bd7
kimmo <hemakim0215@naver.com>
2025-02-15 11:58:20 +0900

    .

--- back/AgitBack/server/child_packets/Packet_User.cs
+++ back/AgitBack/server/child_packets/Packet_User.cs
@@ -3,5 +3,17 @@
 
 namespace AgitBack.AgitPacket
 {
-    
+    public class PacketCreateUserAccount : PacketSuper
+    {
+        public string userAccountId { get; set; } = "";
+        public string userAccountPassword { get; set; } = "";
+
+        public string userAppName { get; set; } = "";
+
+    }
+    public class PacketLoginUser : PacketSuper
+    {
+        public string userAccountId { get; set; } = "";
+        public string userAccountPassword { get; set; } = "";
+    }
 }
