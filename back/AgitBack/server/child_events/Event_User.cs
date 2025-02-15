71333b4b70687dbbbd1c197fd525947d91c41f76
kimmo <hemakim0215@naver.com>
2025-02-15 11:27:17 +0900

    .

---
+++ back/AgitBack/server/child_events/Event_User.cs
@@ -0,0 +1,16 @@
+
+
+
+namespace AgitBack.AgitAsyncEvent
+{
+    public partial class AsyncEventManager
+    {
+        public void Event_LoginUser(string msg)
+        {
+
+            
+        }
+    }
+
+
+}
