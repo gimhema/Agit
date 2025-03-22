b7076ee31b65d3e9b74ce3b728008bf5ac538c5c
kimmo <hemakim0215@naver.com>
2025-03-22 12:33:34 +0900

    .

--- back/AgitBack/server/Server.cs
+++ back/AgitBack/server/Server.cs
@@ -1,15 +1,16 @@
-
 using AgitBack.WebsocketHandler;
+using AgitBack.AgitDB;
+
 
 namespace AgitBack.Server
 {
-    
     public class AgitMainServer
     {
 
         private void Init()
         {
             // Initialize
+            DBConnector.Instance.Start();
             AgitWebSockketHandler.Instance.Init();
         }
 
@@ -24,6 +25,12 @@ namespace AgitBack.Server
             Init();
 
             Run();
+        }
+
+        public void StopServer()
+        {
+            // 서버 종료 시 DB 연결 종료
+            DBConnector.Instance.ExitDB();
         }
     }
 
