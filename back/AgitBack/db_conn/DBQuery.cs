2b520986606fb44b827e1b79eb51f13d8089c165
kimmo <hemakim0215@naver.com>
2025-03-23 11:12:07 +0900

    .

---
+++ back/AgitBack/db_conn/DBQuery.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.Data.Sqlite;
+
+namespace AgitBack.AgitDB
+{
+    public partial class DBConnector
+    {
+        void InsertNewUser() {
+            // pid name userID password
+        }
+
+        void DeleteUser() {
+
+        }
+
+        void UpdateUser() {
+
+        }
+
+        void GetUser() {
+
+        }
+    }
+
+}
